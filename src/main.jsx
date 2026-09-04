f712e6beaa4493be9f65a6c313303c89df5f4d27
Kayran Henrique Reis Teixeira <kayranhenrique2016@gmail.com>
2026-09-04 15:19:34 -0300

    chore(main.jsx), refactor(/components, /_deprecated): Alterações no main para importar as credenciais do keycloack pelo .env, e movendo componentes do login para pastas de arquivados

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ThemeProvider } from './context/ThemeContext.jsx';
+import { AuthProvider } from 'react-oidc-context';
+
+// chaves do keycloack
+const keycloakConfig = {
+  authority: import.meta.env.VITE_KEYCLOAK_AUTHORITY,
+  client_id: import.meta.env.VITE_KEYCLOAK_CLIENT_ID,
+  redirect_uri: import.meta.env.VITE_REDIRECT_URI,
+  response_type: "code"
+};
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <AuthProvider {...keycloakConfig}>
+        <App />
+      </AuthProvider>
     </ThemeProvider>
   </StrictMode>,
 )
